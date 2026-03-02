0feb89bcd772caefde276a389defc86934941073
Yordan Ayvazov <ayvazov.dani@gmail.com>
2026-03-02 12:03:49 +0200

    Update ein.py

--- ein.py
+++ ein.py
@@ -4,6 +4,7 @@ from seleniumbase import SB
 import random
 
 
+
 # headers = {
 #     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 #     'accept-encoding': 'gzip, deflate, br, zstd',
